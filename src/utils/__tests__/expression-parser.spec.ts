b9885a9837e49a082d488f5d716305aa0948335f
Stefan <>
2022-06-01 22:24:35 -0500

    Create an expression parser to enable state usage in story machines

---
+++ src/utils/__tests__/expression-parser.spec.ts
@@ -0,0 +1,177 @@
+import { ExpressionParser } from "../expression-parser";
+
+interface Test {
+  input: string;
+  context?: Record<string, any>;
+  expected: any;
+}
+
+type TestSuite = Record<string, Test>;
+
+function runTests(tests: TestSuite) {
+  for (const [name, test] of Object.entries(tests)) {
+    const { input, expected, context = {} } = test;
+    it(name, () => {
+      const expression = ExpressionParser.tryParse(input);
+      expect(expression.calc(context as any)).toEqual(expected);
+    });
+  }
+}
+
+describe("Expression Parser", () => {
+  describe("Constants", () => {
+    const tests: TestSuite = {
+      "parses integers": {
+        input: "4",
+        expected: 4,
+      },
+      "parses floating point numbers": {
+        input: "1.2",
+        expected: 1.2,
+      },
+      "parses negative numbers": {
+        input: "-4",
+        expected: -4,
+      },
+      "parses strings": {
+        input: '"hello world"',
+        expected: "hello world",
+      },
+      "parses true": {
+        input: "true",
+        expected: true,
+      },
+      "parses false": {
+        input: "false",
+        expected: false,
+      },
+    };
+    runTests(tests);
+  });
+  describe("Variable Reference", () => {
+    const tests: TestSuite = {
+      "references the value corresponding to the access path provided": {
+        input: "$ctx.test",
+        context: { test: "abc123" },
+        expected: "abc123",
+      },
+    };
+    runTests(tests);
+  });
+  describe("Parentheses", () => {
+    const tests: TestSuite = {
+      "references the value wrapped by parentheses": {
+        input: "(3)",
+        expected: 3,
+      },
+      "handles arbitrary nesting": {
+        input: "(((5)))",
+        expected: 5,
+      },
+    };
+    runTests(tests);
+  });
+  describe("Negation", () => {
+    const tests: TestSuite = {
+      "returns the logical opposite of the value": {
+        input: "!false",
+        expected: true,
+      },
+    };
+    runTests(tests);
+  });
+  describe("Math", () => {
+    const tests: TestSuite = {
+      "adds two numbers together": {
+        input: "1 + 3",
+        expected: 4,
+      },
+      "subtracts two numbers": {
+        input: "15 - 3",
+        expected: 12,
+      },
+      "multiplies two numbers": {
+        input: "4 * 6",
+        expected: 24,
+      },
+      "divides two numbers": {
+        input: "57 / 19",
+        expected: 3,
+      },
+      "operates with variable references": {
+        input: "$ctx.num + 5",
+        context: { num: 7 },
+        expected: 12,
+      },
+      "operates with parentheticals": {
+        input: "6 * (1 + 5)",
+        expected: 36,
+      },
+    };
+    runTests(tests);
+  });
+  describe("Logic", () => {
+    const tests: TestSuite = {
+      "supports less than operator": {
+        input: "1 < 3",
+        expected: true,
+      },
+      "supports greater than operator": {
+        input: "1 > 3",
+        expected: false,
+      },
+      "supports the less than or equal to operator": {
+        input: "1 <= 3",
+        expected: true,
+      },
+      "supports the greater than or equal to operator": {
+        input: "1 >= 3",
+        expected: false,
+      },
+      "supports the equality operator": {
+        input: "2 == 2",
+        expected: true,
+      },
+      "supports the inequality operator": {
+        input: "2 != 2",
+        expected: false,
+      },
+      "supports the boolean AND operator": {
+        input: "true && false",
+        expected: false,
+      },
+      "supports the boolean OR operator": {
+        input: "true || false",
+        expected: true,
+      },
+      "operates with variable references": {
+        input: "$ctx.num > 5",
+        context: { num: 7 },
+        expected: true,
+      },
+      "operates with parentheticals": {
+        input: "true && (true || false)",
+        expected: true,
+      },
+    };
+    runTests(tests);
+  });
+  describe("Ternary", () => {
+    const tests: TestSuite = {
+      "returns the first expression when the condition is true": {
+        input: "true ? 1 : 2",
+        expected: 1,
+      },
+      "returns the second expression when the condition is false": {
+        input: "false ? 1 : 2",
+        expected: 2,
+      },
+      "supports complex conditions": {
+        input: '(($ctx.test > 5) && ($ctx.other != 13)) ? "hello" : "goodbye"',
+        context: { test: 7, other: 21 },
+        expected: "hello",
+      },
+    };
+    runTests(tests);
+  });
+});
---
+++ src/utils/expression-parser.ts
@@ -0,0 +1,184 @@
+import { get } from "lodash";
+import Parsimmon, { Parser } from "parsimmon";
+import { Context } from "../types";
+
+interface Expression {
+  calc(context: Context): any;
+}
+
+class Constant implements Expression {
+  constructor(private val: string | number | boolean) {}
+  calc() {
+    return this.val;
+  }
+}
+
+class ContextRef implements Expression {
+  constructor(private dotAccessor: string) {}
+  calc(context: Context) {
+    return get(context, this.dotAccessor);
+  }
+}
+
+class Negation implements Expression {
+  constructor(private expr: Expression) {}
+  calc(context: Context) {
+    return !this.expr.calc(context);
+  }
+}
+
+class Math implements Expression {
+  constructor(
+    private operator: string,
+    private leftOp: Expression,
+    private rightOp: Expression
+  ) {}
+  calc(context: Context) {
+    const leftResult = this.leftOp.calc(context);
+    const rightResult = this.rightOp.calc(context);
+    switch (this.operator) {
+      case "+":
+        return leftResult + rightResult;
+      case "-":
+        return leftResult - rightResult;
+      case "*":
+        return leftResult * rightResult;
+      case "/":
+        return leftResult / rightResult;
+    }
+  }
+}
+
+class Logic implements Expression {
+  constructor(
+    private operator: string,
+    private leftOp: Expression,
+    private rightOp: Expression
+  ) {}
+  calc(context: Context) {
+    const leftResult = this.leftOp.calc(context);
+    const rightResult = this.rightOp.calc(context);
+    switch (this.operator) {
+      case "<":
+        return leftResult < rightResult;
+      case ">":
+        return leftResult > rightResult;
+      case "<=":
+        return leftResult <= rightResult;
+      case ">=":
+        return leftResult >= rightResult;
+      case "==":
+        return leftResult === rightResult;
+      case "!=":
+        return leftResult != rightResult;
+      case "||":
+        return leftResult || rightResult;
+      case "&&":
+        return leftResult && rightResult;
+    }
+  }
+}
+
+class Ternary implements Expression {
+  constructor(
+    private condition: Expression,
+    private trueExpr: Expression,
+    private falseExpr: Expression
+  ) {}
+  calc(context: Context) {
+    const conditionResult = this.condition.calc(context);
+
+    if (conditionResult) {
+      return this.trueExpr.calc(context);
+    } else {
+      return this.falseExpr.calc(context);
+    }
+  }
+}
+
+const BooleanParser = Parsimmon.alt(
+  Parsimmon.string("true"),
+  Parsimmon.string("false")
+).map((str) => str === "true");
+const StringParser = Parsimmon.string('"')
+  .then(Parsimmon.takeWhile((char) => char !== '"'))
+  .skip(Parsimmon.string('"'));
+const NumberParser = Parsimmon.regexp(/-?[0-9]+(\.[0-9]+)?/).map((res) =>
+  Number(res)
+);
+
+const ConstantParser = Parsimmon.alt(
+  StringParser,
+  NumberParser,
+  BooleanParser
+).map((val) => new Constant(val));
+
+const DotAccessorParser = Parsimmon.regexp(/(\.[a-zA-Z0-9_]+)*/i).map(
+  (accessPath) => accessPath.slice(1) // Remove the leading '.' for easier usage
+);
+
+const ContextRefParser = Parsimmon.seqMap(
+  Parsimmon.string("$ctx"),
+  DotAccessorParser,
+  (_, dotAccessor) => new ContextRef(dotAccessor)
+);
+const VariableRefParser: Parser<Expression> = Parsimmon.alt(ContextRefParser);
+
+const ParentheticalParser: Parser<Expression> = Parsimmon.lazy(() =>
+  ExpressionParser.wrap(Parsimmon.string("("), Parsimmon.string(")"))
+);
+
+const NegationParser: Parser<Expression> = Parsimmon.lazy(() =>
+  Parsimmon.seq(Parsimmon.string("!"), ExpressionAtomParser)
+).map(([_, expr]) => new Negation(expr));
+
+const ExpressionAtomParser: Parser<Expression> = Parsimmon.alt(
+  NegationParser,
+  ParentheticalParser,
+  VariableRefParser,
+  ConstantParser
+).trim(Parsimmon.optWhitespace);
+
+const MathParser: Parser<Expression> = Parsimmon.seq(
+  ExpressionAtomParser,
+  Parsimmon.oneOf("+-*/"),
+  ExpressionAtomParser
+).map(([leftOp, operator, rightOp]) => new Math(operator, leftOp, rightOp));
+
+const LogicParser: Parser<Expression> = Parsimmon.seq(
+  ExpressionAtomParser,
+  Parsimmon.alt(
+    Parsimmon.string(">="),
+    Parsimmon.string("<="),
+    Parsimmon.string(">"),
+    Parsimmon.string("<"),
+    Parsimmon.string("=="),
+    Parsimmon.string("!="),
+    Parsimmon.string("||"),
+    Parsimmon.string("&&")
+  ),
+  ExpressionAtomParser
+).map(([leftOp, operator, rightOp]) => new Logic(operator, leftOp, rightOp));
+
+const TernaryParser: Parser<Expression> = Parsimmon.seq(
+  ExpressionAtomParser,
+  Parsimmon.string("?"),
+  ExpressionAtomParser,
+  Parsimmon.string(":"),
+  ExpressionAtomParser
+).map(
+  ([condition, _1, trueExpr, _2, falseExpr]) =>
+    new Ternary(condition, trueExpr, falseExpr)
+);
+
+export const ExpressionParser: Parser<Expression> = Parsimmon.lazy(() =>
+  Parsimmon.alt(TernaryParser, MathParser, LogicParser, ExpressionAtomParser)
+);
+
+export const WrappedExpressionParser: Parser<Expression> =
+  ExpressionParser.trim(Parsimmon.optWhitespace).wrap(
+    Parsimmon.string("{{"),
+    Parsimmon.string("}}")
+  );
+
+export const ManyExpressionParser = ExpressionParser.many();
