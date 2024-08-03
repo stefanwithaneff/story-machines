b96162aaea03e9fcc0d94d9faff508c59e7dba8b
Stefan <8181968+stefanwithaneff@users.noreply.github.com>
2024-08-03 14:16:44 -0500

    Implement init/save/load methods for ImportMachine (#15)
    Co-authored-by: Stefan <>

--- packages/core/src/machines/file-loaders/__tests__/import-machine.spec.ts
+++ packages/core/src/machines/file-loaders/__tests__/import-machine.spec.ts
@@ -2,6 +2,7 @@ import { StoryMachineRuntime } from "../../../runtime";
 import { TestPlayer } from "../../../test-player";
 import { LOAD_FILE } from "../load-file";
 import { GLOBAL_FILES_CONTEXT_KEY } from "../constants";
+import { SaveData } from "../../../types";
 
 const runtime = new StoryMachineRuntime();
 
@@ -42,5 +43,82 @@ describe("ImportMachine", () => {
     });
     expect(player.currentStatus).toEqual("Completed");
     expect(player.currentContext?.test).toEqual("abc");
+  });
+
+  it("saves the data of the imported machine and loads it to the imported machine", () => {
+    const story = `<ImportMachine src="/test/machine" />`;
+    const importedMachine = `
+        <Stateful id="stateful">
+          <InitialState>
+            <Object key="values">
+              <Value key="testNum">5</Value>
+              <Value key="testStr">"hello"</Value>
+            </Object>
+          </InitialState>
+          <EffectHandlers>
+            <EffectHandler type="changeNum" payloadKey="payload">
+              <SetState key="values.testNum">$ctx["payload"]["val"]</SetState>
+            </EffectHandler>
+          </EffectHandlers>
+          <Effect type="changeNum">
+            <Value key="val">18</Value>
+          </Effect>
+          <Wait id="stateful_wait" />
+          <SetGlobalContext key="actualState">$state["values"]</SetGlobalContext>
+        </Stateful>
+      `;
+
+    const expectedState = { values: { testNum: 18, testStr: "hello" } };
+
+    const fileContext = {
+      [GLOBAL_FILES_CONTEXT_KEY]: { "/test/machine": importedMachine },
+    };
+
+    const player = new TestPlayer(runtime, story);
+
+    // Run one effect, Save data and check that it matches
+    const saveData: SaveData = {};
+    player.tick(fileContext).save(saveData);
+
+    expect(player.currentStatus).toEqual("Running");
+    expect(saveData["stateful"]).toEqual({
+      state: expectedState,
+    });
+
+    // Init the story, Load data back in, and see if it picks up where it left off
+    player.init().load(saveData).tick(fileContext);
+    expect(player.currentStatus).toEqual("Completed");
+    expect(player.currentContext?.actualState).toEqual(expectedState.values);
+  });
+
+  it("saves nothing if the machine has not been imported yet", () => {
+    const story = `<ImportMachine src="/test/machine" />`;
+    const importedMachine = `
+        <Stateful id="stateful">
+          <InitialState>
+            <Object key="values">
+              <Value key="testNum">5</Value>
+              <Value key="testStr">"hello"</Value>
+            </Object>
+          </InitialState>
+          <EffectHandlers>
+            <EffectHandler type="changeNum" payloadKey="payload">
+              <SetState key="values.testNum">$ctx["payload"]["val"]</SetState>
+            </EffectHandler>
+          </EffectHandlers>
+          <Effect type="changeNum">
+            <Value key="val">18</Value>
+          </Effect>
+          <Wait id="stateful_wait" />
+          <SetGlobalContext key="actualState">$state["values"]</SetGlobalContext>
+        </Stateful>
+      `;
+    const player = new TestPlayer(runtime, story);
+
+    // Run one effect, Save data and check that it matches
+    const saveData: SaveData = {};
+    player.tick().save(saveData);
+
+    expect(saveData).toEqual({});
   });
 });
--- packages/core/src/machines/file-loaders/import-machine.ts
+++ packages/core/src/machines/file-loaders/import-machine.ts
@@ -1,13 +1,10 @@
-import { Context, ElementTree, Result } from "../../types";
-import { Expression, ExpressionParser } from "../../utils/expression-parser";
-import { updateContext } from "../../utils/scope";
+import { Context, ElementTree, Result, SaveData } from "../../types";
 import {
   StoryMachine,
   StoryMachineAttributes,
   StoryMachineClass,
 } from "../../base-classes";
 import { createDevErrorEffect } from "../effects";
-import { GLOBAL_FILES_CONTEXT_KEY } from "./constants";
 import { addEffectToOutput } from "../../utils/effects";
 import { StaticImplements } from "../../utils/static-implements";
 import { StoryMachineRuntime } from "../../runtime";
@@ -21,12 +18,30 @@ interface ImportMachineAttributes extends StoryMachineAttributes {
 
 @StaticImplements<StoryMachineClass>()
 export class ImportMachine extends StoryMachine<ImportMachineAttributes> {
-  private machineContents: string | undefined;
   private machine: StoryMachine | undefined;
+  private saveData: SaveData | undefined;
 
   static compile(runtime: StoryMachineRuntime, tree: ElementTree) {
     const { src } = tree.attributes;
     return new ImportMachine({ ...tree.attributes, src, runtime });
+  }
+
+  init(): void {
+    this.machine = undefined;
+    this.saveData = undefined;
+  }
+
+  save(saveData: SaveData): void {
+    if (this.machine) {
+      this.machine.save(saveData);
+    }
+  }
+
+  load(saveData: SaveData): void {
+    // Store the saveData on the class because the imported machine has not been compiled yet
+    // We could keep the machine contents in save data, but that makes it impossible to
+    // update the machine once it is saved
+    this.saveData = saveData;
   }
 
   process(context: Context): Result {
@@ -51,6 +66,12 @@ export class ImportMachine extends StoryMachine<ImportMachineAttributes> {
     }
 
     this.machine = machine;
+
+    // Load save data to imported machine if necessary
+    if (this.saveData) {
+      this.machine.load(this.saveData);
+    }
+
     return this.machine.process(context);
   }
 }
